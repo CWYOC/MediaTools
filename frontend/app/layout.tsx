a6621826114981aa02c4d16fd48098b4c0333478
CWYOC <117840941+CWYOC@users.noreply.github.com>
2026-03-23 03:28:12 +0000

    updates

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -11,8 +11,12 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     return (
       <html lang="en">
         <head>
-            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3685958224758369"crossorigin="anonymous">
-            </script>
+            <Script
+            async
+            strategy="afterInteractive"
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3685958224758369"
+            crossOrigin="anonymous"
+            />
         </head>
         <body>
           {children}
